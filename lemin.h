59fc5599a177b524c59b821fab8d0dd5b1d19535
erikagreen7777 <erikagreen7777@gmail.com>
2018-01-30 13:07:34 -0800

    Create lemin.h

---
+++ lemin.h
@@ -0,0 +1,42 @@
+#ifndef LEMIN_H
+# define LEMIN_H
+
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
+# include <stdio.h>
+# include "libft/libft.h"
+# include "libft/printf.h"
+
+# define BUFF_SIZE 256
+
+struct node
+{
+    int vertex;
+    struct node* next;
+};
+
+struct Graph
+{
+    int numVertices;
+    int* visited;
+    struct node** adjLists; // we need int** to store a two dimensional array. Similary, we need struct node** to store an array of Linked lists
+};
+
+typedef struct  s_info
+{
+	char	*start; 
+	char	*end;
+	int		ants;
+    char    **file;
+	char	*name;
+    char    *line;
+}			t_info;
+
+struct node* createNode(int v);
+struct Graph* createGraph(int);
+void addEdge(struct Graph*, int, int);
+void printGraph(struct Graph*);
+void DFS(struct Graph*, int);
+
+#endif
