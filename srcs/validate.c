84b604df1e4719dc1e6e711c4d391f9b32298411
erikagreen7777 <erikagreen7777@gmail.com>
2018-02-01 16:17:11 -0800

    added pipes

--- srcs/validate.c
+++ srcs/validate.c
@@ -44,16 +44,58 @@ void    find_end(t_info *data)
         // room_trim(data, 1);
     if (ft_strcmp(data->startstr, data->endstr) == 0)
         ft_error("ERROR");
-    ft_printf("data->endstr: %s\n", data->endstr);
+    // ft_printf("data->endstr: %s\n", data->endstr);
 }
 
+void    assign_pipes(t_info *data)
+{
+    int i = 0;
+    int j = 0;
+    while (data->file[i])
+    {
+        if (ft_strchr(data->file[i], '-')){
+            printf("helo!\n");
+            data->pipes[j] = ft_strdup(data->file[i]);
+
+        j++;
+    }
+        i++;
+    }
+    data->pipes[j] = NULL;
+    j = -1;
+    while (data->pipes[++j])
+        printf("data->pipes[%d]: %s\n", j, data->pipes[j]);
+}
+
+void    find_pipes(t_info *data)
+{
+    int i;
+    i = 0;
+    int pipecount;
+    pipecount = 0;
+
+    while (data->file[i])
+    {
+        if (ft_strchr(data->file[i], '-'))
+        {
+            pipecount++;
+            printf("Here's the pipe[%d]: %s\n", i, data->file[i]);
+        }
+        i++;
+    }
+    if (pipecount == 0)
+        ft_error("ERROR");
+    data->pipes = (char **)ft_memalloc(sizeof(data->pipes) * pipecount + 1);
+    assign_pipes(data);
+}
 
 void    validate(t_info *data)
 {
 
     find_start(data);
     find_end(data);
-    assign_rooms(data);
+    find_pipes(data);
+    // assign_rooms(data);
 
 }
 
