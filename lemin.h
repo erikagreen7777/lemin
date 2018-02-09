fa56ec328a1a94d01a541e42e957bc948d43111d
erikagreen7777 <erikagreen7777@gmail.com>
2018-02-09 14:13:15 -0800

    memory leaks fixed

--- lemin.h
+++ lemin.h
@@ -57,8 +57,8 @@ t_node 	*createNode(int v);
 t_graph *createGraph(t_info *data);
 void 	addEdge(t_info *data, t_graph *graph, int src, int dest);
 void    addEdgeString(t_graph *graph, char *src, char *dest);
-void 	printGraph(t_graph *graph, t_info *data);
-void 	DFS(t_graph *graph, t_info *data, int targetindex);
+void 	printGraph(t_graph *graph,t_info *data);
+int 	DFS(t_graph *graph, t_info *data, int targetindex);
 void    find_start(t_info *data);
 void    validate(t_info *data);
 int     check_start_room(t_info *data);
@@ -72,5 +72,16 @@ int     find_target_index(t_graph *graph, char *src);
 int     find_start_index(t_graph *graph, char *src);
 int     find_src_index(t_graph *graph, char *src);
 int     find_dest_index(t_graph *graph, char *dest);
+void    free_data(t_info *data/*, t_graph *graph*/);
 
 #endif
+
+//data->solution 88888
+//t_node *newNode
+//data->rooms 8888
+//data->file 88888
+//*graph
+// graph->name 888
+// graph->adjLists
+// graph->visited
+//*data 8888
